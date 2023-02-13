88dc2d2e8825df2f4212bcdfc7b04481a6b8ba5c
ee-ortiz <ee.ortiz@uniandes.edu.co>
2023-02-12 22:55:29 -0500

    Front routing

--- frontend/cloud_entrega_1/src/App.js
+++ frontend/cloud_entrega_1/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  // TODO: Cambiar y retornar El componente routing
   return (
     <div className="App">
       <header className="App-header">
---
+++ frontend/cloud_entrega_1/src/routes/routing.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+const Routing = (props) => {
+    return(
+        <BrowserRouter>
+
+            <Routes>
+                {/* TODO: Realizar las rutas propias de la aplicación */ }
+            </Routes>
+
+        </BrowserRouter>
+
+    );
+}
+
+export default Routing;
+
