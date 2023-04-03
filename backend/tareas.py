bcf1adf12a3f84ff59eac9bbf9801c3052159aca
AmOchoat <am.ochoat@uniandes.edu.co>
2023-04-02 20:20:40 -0500

    App Context

--- backend/tareas.py
+++ backend/tareas.py
@@ -4,37 +4,39 @@ import bz2
 import gzip
 from modelos import *
 import time
+from app import app_context
 
 celery = Celery('tareas', broker='redis://localhost:6379/0')
 
 @celery.task(name='comprimir_zip')
 def comprimir_zip(filename, zipname, new_path,fecha_id):
-    zfile = zipfile.ZipFile(new_path + '/' + zipname, 'w')
-    zfile.write(filename, compress_type = zipfile.ZIP_DEFLATED)
-    zfile.close()
-    tarea_en_cuest=Tarea.query.filter(Tarea.fecha==fecha_id).first()
-    tarea_en_cuest.estado = "processed"
-    db.session.commit()
+    with app_context():
+        zfile = zipfile.ZipFile(new_path + '/' + zipname, 'w')
+        zfile.write(filename, compress_type = zipfile.ZIP_DEFLATED)
+        zfile.close()
+        tarea_en_cuest=Tarea.query.filter(Tarea.fecha==fecha_id).first()
+        tarea_en_cuest.estado = "processed"
+        db.session.commit()
 
 @celery.task(name='comprimir_gzip')
 def comprimir_gzip(filename, zipname, new_path,fecha_id):
-    time.sleep(30)
-    old_file= open(filename)
-    gzipFile = gzip.open(new_path + '/' + zipname, 'w')
-    gzipFile.write(old_file.read())
-    gzipFile.close()
-    tarea_en_cuest=Tarea.query.filter(Tarea.fecha==fecha_id).first()
-    tarea_en_cuest.estado = "processed"
-    db.session.commit()
+    with app_context():
+        old_file= open(filename)
+        gzipFile = gzip.open(new_path + '/' + zipname, 'w')
+        gzipFile.write(old_file.read())
+        gzipFile.close()
+        tarea_en_cuest=Tarea.query.filter(Tarea.fecha==fecha_id).first()
+        tarea_en_cuest.estado = "processed"
+        db.session.commit()
 
 @celery.task(name='comprimir_bz2')
 def comprimir_bz2(filename, zipname, new_path,fecha_id):
-    time.sleep(30)
-    old_file= open(filename)
-    bz2File = open(new_path + '/' + zipname, 'w')
-    bz2File.write(bz2.compress(old_file.read()))
-    bz2File.close()
-    tarea_en_cuest=Tarea.query.filter(Tarea.fecha==fecha_id).first()
-    tarea_en_cuest.estado = "processed"
-    db.session.commit()
+    with app_context():
+        old_file= open(filename)
+        bz2File = open(new_path + '/' + zipname, 'w')
+        bz2File.write(bz2.compress(old_file.read()))
+        bz2File.close()
+        tarea_en_cuest=Tarea.query.filter(Tarea.fecha==fecha_id).first()
+        tarea_en_cuest.estado = "processed"
+        db.session.commit()
 
