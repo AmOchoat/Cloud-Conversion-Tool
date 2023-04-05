d852056337ca8452ecaf0ee1ded90e5e1ee0dbe3
AmOchoat <am.ochoat@uniandes.edu.co>
2023-04-04 21:53:26 -0500

    Update estado tarea

--- backend/tareas.py
+++ backend/tareas.py
@@ -1,42 +1,40 @@
 from celery import Celery
+from sqlalchemy.sql import text
+from sqlalchemy import create_engine
+
 import zipfile
 import bz2
 import gzip
-from modelos import *
-import time
-from app import app_context
+from config import *
 
 celery = Celery('tareas', broker='redis://10.0.1.11:6379/0')
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
 
 @celery.task(name='comprimir_zip')
 def comprimir_zip(filename, zipname, new_path,fecha_id):
-    with app_context():
-        zfile = zipfile.ZipFile(new_path + '/' + zipname, 'w')
-        zfile.write(filename, compress_type = zipfile.ZIP_DEFLATED)
-        zfile.close()
-        tarea_en_cuest=Tarea.query.filter(Tarea.fecha==fecha_id).first()
-        tarea_en_cuest.estado = "processed"
-        db.session.commit()
-
+    zfile = zipfile.ZipFile(new_path + '/' + zipname, 'w')
+    zfile.write(filename, compress_type = zipfile.ZIP_DEFLATED)
+    zfile.close()
+    with engine.connect() as con:
+        sentencia = f"UPDATE tarea SET estado = 'processed' WHERE fecha = '{fecha_id}';"
+        result = con.execute(text(sentencia))
+        
 @celery.task(name='comprimir_gzip')
 def comprimir_gzip(filename, zipname, new_path,fecha_id):
-    with app_context():
-        old_file= open(filename)
-        gzipFile = gzip.open(new_path + '/' + zipname, 'w')
-        gzipFile.write(old_file.read())
-        gzipFile.close()
-        tarea_en_cuest=Tarea.query.filter(Tarea.fecha==fecha_id).first()
-        tarea_en_cuest.estado = "processed"
-        db.session.commit()
+    old_file= open(filename)
+    gzipFile = gzip.open(new_path + '/' + zipname, 'w')
+    gzipFile.write(old_file.read())
+    gzipFile.close()
+    with engine.connect() as con:
+        sentencia = f"UPDATE tarea SET estado = 'processed' WHERE fecha = '{fecha_id}';"
+        result = con.execute(text(sentencia))
 
 @celery.task(name='comprimir_bz2')
 def comprimir_bz2(filename, zipname, new_path,fecha_id):
-    with app_context():
-        old_file= open(filename)
-        bz2File = open(new_path + '/' + zipname, 'w')
-        bz2File.write(bz2.compress(old_file.read()))
-        bz2File.close()
-        tarea_en_cuest=Tarea.query.filter(Tarea.fecha==fecha_id).first()
-        tarea_en_cuest.estado = "processed"
-        db.session.commit()
-
+    old_file= open(filename)
+    bz2File = open(new_path + '/' + zipname, 'w')
+    bz2File.write(bz2.compress(old_file.read()))
+    bz2File.close()
+    with engine.connect() as con:
+        sentencia = f"UPDATE tarea SET estado = 'processed' WHERE fecha = '{fecha_id}';"
+        result = con.execute(text(sentencia))
