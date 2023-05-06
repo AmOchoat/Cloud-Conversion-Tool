c5dc082c6e17fa0a13933aad799c4549dd6a0d68
AmOchoat <am.ochoat@uniandes.edu.co>
2023-05-06 12:33:46 -0500

    Dockerfile

--- worker/tareas.py
+++ worker/tareas.py
@@ -10,6 +10,29 @@ import gzip
 from config import *
 from io import BytesIO
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+OUR_HOST = os.getenv("DB_HOST", "127.0.0.1")
+OUR_DB = os.getenv("DB_DB", "tasks")
+OUR_USER = os.getenv("DB_USER", "tasks")
+OUR_PORT = os.getenv("DB_PORT", "5432")
+OUR_PW = os.getenv("DB_PW", "tasks")
+OUR_SECRET = os.getenv("SECRET", "tasks")
+OUR_JWTSECRET = os.getenv("JWTSECRET", "tasks")
+OUR_ALGO = os.getenv("JWT_ALGORITHM" ,"HS256")
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'entrega-3-CloudStorage.json'
+
+DEBUG = False
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@{}:{}/{}'.format(
+    OUR_USER, OUR_PW, OUR_HOST, OUR_PORT, OUR_DB)
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+JWT_SECRET_KEY = OUR_JWTSECRET
+SECRET_KEY = OUR_SECRET
+JWT_ALGORITHM= OUR_ALGO
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 
