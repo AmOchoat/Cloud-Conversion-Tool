f77d034272ad2ac42626b5d71c7a80f2b543d68d
AmOchoat <am.ochoat@uniandes.edu.co>
2023-05-21 14:57:53 -0500

    Change

--- worker/tareas.py
+++ worker/tareas.py
@@ -1,9 +1,7 @@
-
 from sqlalchemy.sql import text
 from sqlalchemy import create_engine
 from google.cloud import pubsub_v1
 from google.cloud import storage
-from flask import Flask, request
 
 import zipfile
 import bz2
@@ -12,8 +10,6 @@ from io import BytesIO
 
 import os
 from dotenv import load_dotenv
-
-app = Flask(__name__)
 
 load_dotenv()
 OUR_HOST = os.getenv("DB_HOST", "127.0.0.1")
@@ -36,140 +32,117 @@ JWT_ALGORITHM= OUR_ALGO
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 
-#storage_client = storage.Client.from_service_account_json("entrega-3-CloudStorage.json")
+storage_client = storage.Client.from_service_account_json("entrega-3-CloudStorage.json")
 bucket_name = "cloud-entrega-4"
 
-@app.route('/', methods=['POST'])
-def index():
-    print("Prueba de Flask")
-    envelope = request.get_json()
-    if not envelope:
-        msg = "no Pub/Sub message received"
-        print(f"error: {msg}")
-        return f"Bad Request: {msg}", 400
+def recibir_mensaje(pubsub_subscription):
+    # Crea una instancia del cliente de Pub/Sub con las credenciales
+    subscriber = pubsub_v1.SubscriberClient.from_service_account_json("pub_sub.json")
+    
+    # Crea una función de callback para procesar los mensajes recibidos
+    def callback(message):
+        # Extrae los parámetros de la tarea del mensaje
+        mensaje_split = message.data.decode().split(' ')
+        tarea = mensaje_split[0]
+        filename = mensaje_split[1]
+        zipname = mensaje_split[2]
+        fecha_id = mensaje_split[3]
+        
+        # Ejecuta la tarea correspondiente
+        if tarea == 'comprimir_zip':
+            comprimir_zip(bucket_name, filename, zipname, fecha_id)
+        elif tarea == 'comprimir_gzip':
+            comprimir_gzip(filename, zipname, bucket_name, fecha_id)
+        elif tarea == 'comprimir_bz2':
+            comprimir_bz2(filename, zipname, bucket_name, fecha_id)
 
-    if not isinstance(envelope, dict) or "message" not in envelope:
-        msg = "invalid Pub/Sub message format"
-        print(f"error: {msg}")
-        return f"Bad Request: {msg}", 400
+        message.ack()
 
-    pubsub_message = envelope["message"]
+    
+    # Crea una instancia de suscripción
+    subscription_path = subscriber.subscription_path("entrega-3-cloud", pubsub_subscription)
+    streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+    
+    # Inicia el bucle de recepción de mensajes
+    with subscriber:
+        try:
+            # Espera hasta que se cierre el futuro de recepción de mensajes
+            streaming_pull_future.result()
+        except Exception as e:
+            # Maneja cualquier error ocurrido durante la recepción de mensajes
+            print(f'Error al recibir mensajes: {e}')
 
-    print(pubsub_message)
 
-    return("Corriendo", 204)
-#
-#
-#def recibir_mensaje(pubsub_subscription):
-#    # Crea una instancia del cliente de Pub/Sub con las credenciales
-#    subscriber = pubsub_v1.SubscriberClient.from_service_account_json("pub_sub.json")
-#    
-#    # Crea una función de callback para procesar los mensajes recibidos
-#    def callback(message):
-#        # Extrae los parámetros de la tarea del mensaje
-#        mensaje_split = message.data.decode().split(' ')
-#        tarea = mensaje_split[0]
-#        filename = mensaje_split[1]
-#        zipname = mensaje_split[2]
-#        fecha_id = mensaje_split[3]
-#        
-#        # Ejecuta la tarea correspondiente
-#        if tarea == 'comprimir_zip':
-#            comprimir_zip(bucket_name, filename, zipname, fecha_id)
-#        elif tarea == 'comprimir_gzip':
-#            comprimir_gzip(filename, zipname, bucket_name, fecha_id)
-#        elif tarea == 'comprimir_bz2':
-#            comprimir_bz2(filename, zipname, bucket_name, fecha_id)
-#
-#        message.ack()
-#
-#    
-#    # Crea una instancia de suscripción
-#    subscription_path = subscriber.subscription_path("entrega-3-cloud", pubsub_subscription)
-#    streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
-#    
-#    # Inicia el bucle de recepción de mensajes
-#    with subscriber:
-#        try:
-#            # Espera hasta que se cierre el futuro de recepción de mensajes
-#            streaming_pull_future.result()
-#        except Exception as e:
-#            # Maneja cualquier error ocurrido durante la recepción de mensajes
-#            print(f'Error al recibir mensajes: {e}')
-#
-#
-#def comprimir_zip(bucket_name, filename, zipname, fecha_id):
-#    bucket = storage_client.get_bucket(bucket_name)
-#
-#    print("zipname:", zipname)
-#    print("filename:", filename)
-#    
-#    blob = bucket.blob(filename)
-#    
-#    with BytesIO() as zip_buffer:
-#        with zipfile.ZipFile(zip_buffer, "w", compression=zipfile.ZIP_DEFLATED) as zip_file:
-#            data = blob.download_as_string()
-#            zip_file.writestr(filename, data)
-#
-#        blob_zip = bucket.blob(zipname)
-#        blob_zip.upload_from_string(zip_buffer.getvalue())
-#
-#    with engine.connect() as con:
-#        fecha_processed = fecha_id.replace("T", " ")
-#        sentencia = f"UPDATE tarea SET estado = 'processed' WHERE fecha = '{fecha_processed}';"
-#        con.execute(text(sentencia))
-#        con.commit()
-#        
-#def comprimir_gzip(bucket_name, filename, zipname, fecha_id):
-#    bucket = storage_client.get_bucket(bucket_name)
-#
-#    print("zipname:", zipname)
-#    print("filename:", filename)
-#
-#
-#    blob = bucket.blob(filename)
-#
-#    with BytesIO() as zip_buffer:
-#        with gzip.GzipFile(mode='wb', fileobj=zip_buffer) as zip_file:
-#            data = blob.download_as_string()
-#            zip_file.write(data)
-#
-#        blob_zip = bucket.blob(zipname)
-#        blob_zip.upload_from_string(zip_buffer.getvalue())
-#
-#    with engine.connect() as con:
-#        fecha_processed = fecha_id.replace("T", " ")
-#        sentencia = f"UPDATE tarea SET estado = 'processed' WHERE fecha = '{fecha_processed}';"
-#        con.execute(text(sentencia))
-#        con.commit()
-#
-#
-#def comprimir_bz2(bucket_name, filename, zipname, fecha_id):
-#
-#    bucket = storage_client.get_bucket(bucket_name)
-#
-#    print("zipname:", zipname)
-#    print("filename:", filename)
-#
-#
-#    blob = bucket.blob(filename)
-#
-#    with BytesIO() as zip_buffer:
-#        with bz2.BZ2File(mode='wb', filename=zip_buffer) as zip_file:
-#            data = blob.download_as_string()
-#            zip_file.write(data)
-#
-#        blob_zip = bucket.blob(zipname)
-#        blob_zip.upload_from_string(zip_buffer.getvalue())
-#
-#    with engine.connect() as con:
-#        fecha_processed = fecha_id.replace("T", " ")
-#        sentencia = f"UPDATE tarea SET estado = 'processed' WHERE fecha = '{fecha_processed}';"
-#        con.execute(text(sentencia))
-#        con.commit()
-#
-#
-#if __name__ == "__main__":
-#    recibir_mensaje("compresion_archivo-sub")
-#
-#
+def comprimir_zip(bucket_name, filename, zipname, fecha_id):
+    bucket = storage_client.get_bucket(bucket_name)
+
+    print("zipname:", zipname)
+    print("filename:", filename)
+    
+    blob = bucket.blob(filename)
+    
+    with BytesIO() as zip_buffer:
+        with zipfile.ZipFile(zip_buffer, "w", compression=zipfile.ZIP_DEFLATED) as zip_file:
+            data = blob.download_as_string()
+            zip_file.writestr(filename, data)
+
+        blob_zip = bucket.blob(zipname)
+        blob_zip.upload_from_string(zip_buffer.getvalue())
+
+    with engine.connect() as con:
+        fecha_processed = fecha_id.replace("T", " ")
+        sentencia = f"UPDATE tarea SET estado = 'processed' WHERE fecha = '{fecha_processed}';"
+        con.execute(text(sentencia))
+        con.commit()
+        
+def comprimir_gzip(bucket_name, filename, zipname, fecha_id):
+    bucket = storage_client.get_bucket(bucket_name)
+
+    print("zipname:", zipname)
+    print("filename:", filename)
+
+
+    blob = bucket.blob(filename)
+
+    with BytesIO() as zip_buffer:
+        with gzip.GzipFile(mode='wb', fileobj=zip_buffer) as zip_file:
+            data = blob.download_as_string()
+            zip_file.write(data)
+
+        blob_zip = bucket.blob(zipname)
+        blob_zip.upload_from_string(zip_buffer.getvalue())
+
+    with engine.connect() as con:
+        fecha_processed = fecha_id.replace("T", " ")
+        sentencia = f"UPDATE tarea SET estado = 'processed' WHERE fecha = '{fecha_processed}';"
+        con.execute(text(sentencia))
+        con.commit()
+
+
+def comprimir_bz2(bucket_name, filename, zipname, fecha_id):
+
+    bucket = storage_client.get_bucket(bucket_name)
+
+    print("zipname:", zipname)
+    print("filename:", filename)
+
+
+    blob = bucket.blob(filename)
+
+    with BytesIO() as zip_buffer:
+        with bz2.BZ2File(mode='wb', filename=zip_buffer) as zip_file:
+            data = blob.download_as_string()
+            zip_file.write(data)
+
+        blob_zip = bucket.blob(zipname)
+        blob_zip.upload_from_string(zip_buffer.getvalue())
+
+    with engine.connect() as con:
+        fecha_processed = fecha_id.replace("T", " ")
+        sentencia = f"UPDATE tarea SET estado = 'processed' WHERE fecha = '{fecha_processed}';"
+        con.execute(text(sentencia))
+        con.commit()
+
+
+if __name__ == "__main__":
+    recibir_mensaje("compresion_archivo-sub")
