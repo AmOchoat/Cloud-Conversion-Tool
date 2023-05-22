64f1624cd26911d79679a5a85cf3b3cb02a5bb9f
ee-ortiz <ee.ortiz@uniandes.edu.co>
2023-05-21 21:25:49 -0500

    XD

--- worker/tareas.py
+++ worker/tareas.py
@@ -1,7 +1,12 @@
 from sqlalchemy.sql import text
 from sqlalchemy import create_engine
-from google.cloud import pubsub_v1
 from google.cloud import storage
+import base64
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
 
 import zipfile
 import bz2
@@ -35,42 +40,33 @@ engine = create_engine(SQLALCHEMY_DATABASE_URI)
 storage_client = storage.Client.from_service_account_json("entrega-3-CloudStorage.json")
 bucket_name = "cloud-entrega-4"
 
-def recibir_mensaje(pubsub_subscription):
-    # Crea una instancia del cliente de Pub/Sub con las credenciales
-    subscriber = pubsub_v1.SubscriberClient.from_service_account_json("pub_sub.json")
-    
-    # Crea una función de callback para procesar los mensajes recibidos
-    def callback(message):
-        # Extrae los parámetros de la tarea del mensaje
-        mensaje_split = message.data.decode().split(' ')
-        tarea = mensaje_split[0]
-        filename = mensaje_split[1]
-        zipname = mensaje_split[2]
-        fecha_id = mensaje_split[3]
-        
-        # Ejecuta la tarea correspondiente
-        if tarea == 'comprimir_zip':
-            comprimir_zip(bucket_name, filename, zipname, fecha_id)
-        elif tarea == 'comprimir_gzip':
-            comprimir_gzip(filename, zipname, bucket_name, fecha_id)
-        elif tarea == 'comprimir_bz2':
-            comprimir_bz2(filename, zipname, bucket_name, fecha_id)
 
-        message.ack()
+@app.route("/", methods=["POST"])
+def recibir_mensaje():
+
+    envelope = request.get_json()
+    if not envelope:
+        msg = "no Pub/Sub message received"
+        print(f"error: {msg}")
+        return f"Bad Request: {msg}", 400
+
+    if not isinstance(envelope, dict) or "message" not in envelope:
+        msg = "invalid Pub/Sub message format"
+        print(f"error: {msg}")
+        return f"Bad Request: {msg}", 400
+
+    pubsub_message = envelope["message"]
+
+    name = "World"
+    if isinstance(pubsub_message, dict) and "data" in pubsub_message:
+        name = base64.b64decode(pubsub_message["data"]).decode("utf-8").strip()
 
     
-    # Crea una instancia de suscripción
-    subscription_path = subscriber.subscription_path("entrega-3-cloud", pubsub_subscription)
-    streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
-    
-    # Inicia el bucle de recepción de mensajes
-    with subscriber:
-        try:
-            # Espera hasta que se cierre el futuro de recepción de mensajes
-            streaming_pull_future.result()
-        except Exception as e:
-            # Maneja cualquier error ocurrido durante la recepción de mensajes
-            print(f'Error al recibir mensajes: {e}')
+    print(f"Hello {name}!")
+
+    return ("", 204)
+
+
 
 
 def comprimir_zip(bucket_name, filename, zipname, fecha_id):
@@ -142,7 +138,3 @@ def comprimir_bz2(bucket_name, filename, zipname, fecha_id):
         sentencia = f"UPDATE tarea SET estado = 'processed' WHERE fecha = '{fecha_processed}';"
         con.execute(text(sentencia))
         con.commit()
-
-
-if __name__ == "__main__":
-    recibir_mensaje("compresion_archivo-sub")
